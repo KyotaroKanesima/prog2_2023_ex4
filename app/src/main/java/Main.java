a9b827dc8e6dea5ca9945da9fa63252de80be18c
Kyotaro Kaneshima <e235727@ie.u-ryukyu.ac.jp>
2023-12-19 16:07:50 +0900

    third commit

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,10 +1,14 @@
 import ex4.Animal;
+import ex4.Cat;
 
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal("animal");
+        Cat cat = new Cat("たま");
         System.out.println(animal.getName());
+        System.out.println(cat.getName());
         animal.makeSound();
+        cat.makeSound();
     }
     
 }
