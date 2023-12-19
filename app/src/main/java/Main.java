a4f6905459f7063c2b58bfc36b516133098a5937
Kyotaro Kaneshima <e235727@ie.u-ryukyu.ac.jp>
2023-12-19 15:57:25 +0900

    second commit

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,3 +1,10 @@
+import ex4.Animal;
+
 public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("animal");
+        System.out.println(animal.getName());
+        animal.makeSound();
+    }
     
 }
---
+++ app/src/main/java/ex4/Animal.java
@@ -0,0 +1,19 @@
+package ex4;
+
+public class Animal {
+    private String name;
+
+
+    public Animal(String _name){
+        this.name = _name;
+    }
+
+    public void setName(String name){this.name = name;}
+
+    public String getName(){
+        return this.name;
+    }
+    public void makeSound(){
+        System.out.println(getName() + "が鳴いている");
+    }
+}
