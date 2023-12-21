df928330e356729e9d7c674883b7dfa07d505234
Kyotaro Kaneshima <e235727@ie.u-ryukyu.ac.jp>
2023-12-21 13:32:25 +0900

    seventh commit

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,7 +1,8 @@
 import ex4.Animal;
 import ex4.Bird;
 import ex4.Cat;
-import ex4.Doc;
+import ex4.Dog;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -14,16 +15,34 @@ public class Main {
         System.out.println(cat.getName());
         cat.makeSound();
         //演習4.4
-        Doc doc = new Doc("ポチ");
-        System.out.println(doc.getName());
-        doc.makeSound();
-        doc.swim();
+        Dog dog = new Dog("ポチ");
+        System.out.println(dog.getName());
+        dog.makeSound();
+        dog.swim();
         //演習4.5
         Bird bird = new Bird("チュン太朗");
         System.out.println(bird.getName());
         bird.makeSound();
         bird.fly();
         
+        /** 
+        //演習4.6
+        Animal[] animals = new Animal[4];
+        animals[0] = new Animal("オリジナル動物");
+        animals[1] = new Dog("猫");
+        animals[2] = new Cat("ポチ");
+        animals[3] = new Bird("旧Twitter");
+
+        for (Animal animal : animals) {
+            animal.makeSound();
+            if (animal instanceof Flyable) {
+                ((Flyable) animal).fly();
+            }
+            if (animal instanceof Swimmable) {
+                ((Swimmable) animal).swim();
+            }
+        }
+        */
         
     }
     
